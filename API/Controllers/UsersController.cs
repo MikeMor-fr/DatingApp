0001c4186deaaa77be4485cf70b323e63a64bad7
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2020-12-16 22:35:38 +0100

    making requests asynchronous

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,16 +21,16 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async  Task <ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
             return users;
         }
         
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task <ActionResult<AppUser>> GetUser(int id)
         {
-            var user = _context.Users.Find(id);
+            var user = await _context.Users.FindAsync(id);
             return user;
         }
     }
